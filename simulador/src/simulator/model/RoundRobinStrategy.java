c2a86ede779ef605bf87d7623051412d04173276
Paula S. de la Nieta <36267202+Enolah@users.noreply.github.com>
2020-03-01 12:20:35 +0100

    Round Robin Strategy

--- simulador/src/simulator/model/RoundRobinStrategy.java
+++ simulador/src/simulator/model/RoundRobinStrategy.java
@@ -1,5 +1,29 @@
 package simulator.model;
 
+import java.util.List;
+
 public class RoundRobinStrategy implements LigthSwitchingStrategy{
+
+	private int timeSlot;
+	public RoundRobinStrategy(int timeSlot){
+		super();
+		this.timeSlot= timeSlot;
+	}
+
+	@Override
+	public int chooseNextGreen(List<Road> roads, List<List<Vehicle>> qs, int currGreen, int lastSwitchingTime,
+			int currTime) {
+		int indice =-1;
+		if (qs==null);
+		else{
+			if (currGreen==-1){
+				indice = 0;
+			}
+			//3 y 4
+			else if(lastSwitchingTime<timeSlot)
+				indice= (currGreen+1)%roads.size();
+		}
+		return indice;
+	}
 
 }
