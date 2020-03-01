6c643cc459f3ce3f703771ddc284e5a6780753e0
Paula S. de la Nieta <36267202+Enolah@users.noreply.github.com>
2020-03-01 13:32:23 +0100

    Estrategia de colas

--- simulador/src/simulator/model/MoveAllStrategy.java
+++ simulador/src/simulator/model/MoveAllStrategy.java
@@ -1,13 +1,16 @@
 package simulator.model;
 
+import java.util.Collections;
 import java.util.List;
 
 public class MoveAllStrategy implements DequeuingStrategy{
 
+	
 	@Override
 	public List<Vehicle> dequeue(List<Vehicle> q) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Vehicle> a = null;
+		Collections.copy(a, q); 
+		return a;
 	}
 
 
--- simulador/src/simulator/model/MoveFirstStrategy.java
+++ simulador/src/simulator/model/MoveFirstStrategy.java
@@ -1,13 +1,20 @@
 package simulator.model;
 
+import java.util.Collections;
 import java.util.List;
 
 public class MoveFirstStrategy implements DequeuingStrategy{
 
 	@Override
 	public List<Vehicle> dequeue(List<Vehicle> q) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Vehicle> a = null;
+		Collections.copy(a, q); 
+		for (int i=1; i<=a.size(); i++){
+			if(a.get(i)!=null){
+				a.remove(i);
+			}
+		}
+		return a;
 	}
 
 }
