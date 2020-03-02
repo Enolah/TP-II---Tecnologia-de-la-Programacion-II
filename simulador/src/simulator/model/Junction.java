68d46f479fbf6315733e3196992b6e8e8c56f942
Paula S. de la Nieta <36267202+Enolah@users.noreply.github.com>
2020-03-02 20:52:45 +0100

    RoadMap inicio

--- simulador/src/simulator/model/Junction.java
+++ simulador/src/simulator/model/Junction.java
@@ -1,16 +1,18 @@
 package simulator.model;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class Junction extends SimulatedObject{
 
 	
 	private List<Road> list;
-	private Map<Junction,Road> map;
-	private List<List<Vehicle>> ListQ;
+	private Map<Junction,Road> map; //carrteras salientes
+	private List<List<Vehicle>> listQ;
 	private int currGreen; //indice semafoto verde??
 	private int lastGreen; //ultimo paso de cambio de semaforo?
 	private LigthSwitchingStrategy lsStrategy;
@@ -35,23 +37,53 @@ public class Junction extends SimulatedObject{
 	
 	void addIncommingRoad(Road r){
 		list.add(r);
+		LinkedList link = new LinkedList();
+		link.add(r);
+		listQ.add(link);
+		//TODO completar
+		
 	}
 	
-	void addOutGoingRoad(Road r){}
+	void addOutGoingRoad(Road r){
+	
+		map.put(r.getDest(), r);
+	}
+	
 	void enter (Vehicle v){
+		v.getRoad().enter(v);
 		
 	}
+	
 	Road roadTo (Junction j){
 		
+		Road r= map.get(j);
+		return r;
+	}
+	
+	@Override
+	void advance(int time) {
+		
+		dqStrategy.dequeue(listQ);
+		lsStrategy.chooseNextGreen(list, listQ, time, lastSwitchingTime, currTime);
+		
+	}
+	
+	public JSONObject report(){
+		
+		JSONObject jo1 = new JSONObject();
+		jo1.put("id", _id);
+		if (currGreen==-1)
+			jo1.put("green", list.get(currGreen));
+		else
+			jo1.put("green", "none");
+		
+		JSONArray jo2= new JSONArray();
+		for (Road listQ : list) {//mal
+			
+		}
 		
 		return null;
 	}
-	public JSONObject report(){
-		return null;}
 
-	@Override
-	void advance(int time) {
-		// TODO Auto-generated method stub
-		
-	};
+
 }
--- simulador/src/simulator/model/RoadMap.java
+++ simulador/src/simulator/model/RoadMap.java
@@ -16,12 +16,31 @@ public class RoadMap {
 	
 	public RoadMap(List<Junction> listJ, List<Road> listR,List<Vehicle> listV,Map <String,Junction> mapJ,Map <String,Road> mapR,Map <String,Vehicle> mapV){
 		this.listJ=listJ;
-		//etc
+		this.listR= listR;
+		this.listV= listV;
+		this.mapJ= mapJ;
+		this.mapR=mapR;
+		this.mapV=mapV;
 	}
-	//TODO poner public
-	void addJunction(Junction j){};
-	void addRoad(Road r){};
-	void addVehicle (Vehicle v){};
+	
+	
+	void addJunction(Junction j){
+		listJ.add(j);
+		mapJ.put(j._id, j);
+	}
+	
+	void addRoad(Road r){
+		listR.add(r);
+		mapR.putIfAbsent(r._id, r);//(i)
+		if (mapJ.containsKey(r.getSrc()._id));//(iiççç9
+		else if (mapJ.containsKey(r.getDest()._id));
+		
+	}
+	
+	void addVehicle (Vehicle v){
+		listV.add(v);
+		
+	}
 	public Junction getJunction(String id){
 		return null;}
 	public Road getROad(String id){
