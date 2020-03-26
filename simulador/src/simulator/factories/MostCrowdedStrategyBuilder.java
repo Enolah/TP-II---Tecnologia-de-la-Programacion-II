2710387b3730e69c0239e831593e7caf0ca2b807
Paula <paulsa07@ucm.es>
2020-03-26 03:14:23 +0100

    Factorias

--- simulador/src/simulator/factories/MostCrowdedStrategyBuilder.java
+++ simulador/src/simulator/factories/MostCrowdedStrategyBuilder.java
@@ -1,7 +1,26 @@
 package simulator.factories;
 
+import org.json.JSONObject;
 
+import simulator.model.LigthSwitchingStrategy;
 
-public class MostCrowdedStrategyBuilder {
+public class MostCrowdedStrategyBuilder extends Builder<LigthSwitchingStrategy>{
+
+	MostCrowdedStrategyBuilder(String type) {
+		super(type);
+		
+	}
+
+	@Override
+	protected LigthSwitchingStrategy createTheInstance(JSONObject data) {
+		JSONObject jo1 = new JSONObject();
+		data.put("type", _type);
+		jo1.put("timeslot", 1);
+		data.put("data", jo1);
+		
+		return (LigthSwitchingStrategy)data;
+	}
+	
+	
 
 }
--- simulador/src/simulator/factories/RoundRobinStrategyBuilder.java
+++ simulador/src/simulator/factories/RoundRobinStrategyBuilder.java
@@ -1,5 +1,25 @@
 package simulator.factories;
 
-public class RoundRobinStrategyBuilder {
+import org.json.JSONObject;
+
+import simulator.model.LigthSwitchingStrategy;
+
+public class RoundRobinStrategyBuilder extends Builder<LigthSwitchingStrategy>{
+
+	RoundRobinStrategyBuilder(String type) {
+		super(type);
+	
+	}
+
+	@Override
+	protected LigthSwitchingStrategy createTheInstance(JSONObject data) {
+		
+		JSONObject jo1 = new JSONObject();
+		data.put("type", _type);
+		jo1.put("timeslot", 1);
+		data.put("data", jo1);
+		
+		return (LigthSwitchingStrategy)data;
+	}
 
 }
