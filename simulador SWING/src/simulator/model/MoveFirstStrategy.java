c711756ff4f9f0cd6f477eba8e75203daf48c79f
Paula <paulsa07@ucm.es>
2020-04-20 23:57:55 +0200

    modificaciones errores de la practica 1

--- simulador SWING/src/simulator/model/MoveFirstStrategy.java
+++ simulador SWING/src/simulator/model/MoveFirstStrategy.java
@@ -8,17 +8,9 @@ public class MoveFirstStrategy implements DequeuingStrategy {
 	@Override
 	public List<Vehicle> dequeue(List<Vehicle> q) {
 		List<Vehicle> a = new LinkedList<Vehicle>();
-		if (q.size() != 0) {
-			a.add(q.get(0));
-	
-			for (int i = 1; i < a.size(); i++) { 
-		// elimino todos los elementos de la lista, excepto el primero							
-				if (a.get(i) != null) {
-					a.remove(i);
-				}
-			}
-		}
+		a.add(q.get(0));
 		return a;
 	}
+
 
 }
