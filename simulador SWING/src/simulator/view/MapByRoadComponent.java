d0e0239e5168640ae47ae07c675677ac52b54649
Paula <paulsa07@ucm.es>
2020-04-28 12:53:40 +0200

    map by road he añadido metodos principales

--- simulador SWING/src/simulator/view/MapByRoadComponent.java
+++ simulador SWING/src/simulator/view/MapByRoadComponent.java
@@ -1,7 +1,11 @@
 package simulator.view;
 
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
+import javax.imageio.ImageIO;
 import javax.swing.JComponent;
 
 import simulator.control.Controller;
@@ -11,10 +15,36 @@ import simulator.model.TrafficSimObserver;
 
 public class MapByRoadComponent extends JComponent implements TrafficSimObserver{
 
-	public MapByRoadComponent(Controller _ctrl) {
-		// TODO Auto-generated constructor stub
+	private static final long serialVersionUID = 1L;
+	
+	private RoadMap _map;
+	private Image _car;
+
+	MapByRoadComponent(Controller ctrl) {
+		initGUI();
+		ctrl.addObserver(this);
 	}
 
+	private void initGUI() {
+		_car = loadImage("car.png");
+		
+	}
+
+	// loads an image from a file
+	private Image loadImage(String img) {
+			Image i = null;
+			try {
+				return ImageIO.read(new File("resources/icons/" + img));
+			} catch (IOException e) {
+			}
+			return i;
+		}
+		
+	public void update(RoadMap map) {
+			_map = map;
+			repaint();
+	}
+	
 	@Override
 	public void onAdvanceStart(RoadMap map, List<Event> events, int time) {
 		// TODO Auto-generated method stub
