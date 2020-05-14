63abbf9c00e48b846eb1e34fb6109e07592ddafe
Bruno <Bruno@DESKTOP-VCEEL1T>
2020-05-14 20:46:38 +0200

    Control de excepciones en ControlPanel

--- simulador SWING/src/simulator/view/ControlPanel.java
+++ simulador SWING/src/simulator/view/ControlPanel.java
@@ -30,41 +30,38 @@ import simulator.model.TrafficSimObserver;
 import simulator.model.Vehicle;
 import simulator.model.Weather;
 
-public class ControlPanel extends JPanel implements TrafficSimObserver{
-
+public class ControlPanel extends JPanel implements TrafficSimObserver {
 
 	private static final long serialVersionUID = 1L;
 	private Controller _ctrl;
 	private JButton btnCargaEventos, btnCambiaClase, btnCambiaTiempo, btnPlay, btnStop, btnExit;
 	private JSpinner spinTicks;
 	private boolean _stopped;
-	
-	//atributos de los ON
+
+	// atributos de los ON
 	private RoadMap map;
 	private List<Event> listE;
 	private int ticks;
-	
-	//atributos para las clases change
+
+	// atributos para las clases change
 	private List<Vehicle> listV;
 	private List<Road> listR;
 
 	public ControlPanel(Controller ctrl) {
 		this._ctrl = ctrl;
 		this._stopped = false;
-		listV=new ArrayList<Vehicle>();
-		listR=new ArrayList<Road>();
+		listV = new ArrayList<Vehicle>();
+		listR = new ArrayList<Road>();
 		initGUI();
 		_ctrl.addObserver(this);
 	}
 
-	private void initGUI(){
-	
-		
-		
-		ImageIcon icon= new ImageIcon("resources/icons/open.png");
-		this.btnCargaEventos = new JButton(icon);
+	private void initGUI() {
+
+		// ImageIcon icon = new ImageIcon("resources/icons/open.png");
+		this.btnCargaEventos = new JButton(new ImageIcon("resources/icons/open.png"));
 		this.btnCargaEventos.addActionListener(new ActionListener() {
-		
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				try {
@@ -76,86 +73,93 @@ public class ControlPanel extends JPanel implements TrafficSimObserver{
 
 			}
 		});
-		
 
-		ImageIcon icon1= new ImageIcon("resources/icons/co2class.png");
-		this.btnCambiaClase = new JButton(icon1);
+		// ImageIcon icon1 = new ImageIcon("resources/icons/co2class.png");
+		this.btnCambiaClase = new JButton(new ImageIcon("resources/icons/co2class.png"));
 		this.btnCambiaClase.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				cambiaClase();
-				
+				try {
+					cambiaClase();
+				} catch (NullPointerException ex) {
+					ex.printStackTrace();
+				}
+
 			}
 		});
-	
-		ImageIcon icon2= new ImageIcon("resources/icons/weather.png");
-		this.btnCambiaTiempo = new JButton(icon2);
+
+		// ImageIcon icon2 = new ImageIcon("resources/icons/weather.png");
+		this.btnCambiaTiempo = new JButton(new ImageIcon("resources/icons/weather.png"));
 		this.btnCambiaTiempo.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				cambiaTiempo();
-				
+				try {
+					cambiaTiempo();
+				} catch (NullPointerException ex) {
+					ex.printStackTrace();
+				}
+
 			}
 		});
-	
-		ImageIcon icon3= new ImageIcon("resources/icons/run.png");
-		this.btnPlay = new JButton(icon3);
+
+		// ImageIcon icon3 = new ImageIcon("resources/icons/run.png");
+		this.btnPlay = new JButton(new ImageIcon("resources/icons/run.png"));
 		this.btnPlay.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				_stopped=false;
-				run_sim((int) spinTicks.getValue()); //Provisional - Tiene que coger el valor de ticks
-				
+				_stopped = false;
+				ticks = (int) spinTicks.getValue();
+				run_sim(ticks); // Provisional - Tiene que coger el valor de ticks
+
 			}
 		});
-	
-		ImageIcon icon4= new ImageIcon("resources/icons/stop.png");
-		this.btnStop = new JButton(icon4);
+
+		// ImageIcon icon4 = new ImageIcon("resources/icons/stop.png");
+		this.btnStop = new JButton(new ImageIcon("resources/icons/stop.png"));
 		this.btnStop.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				stop();
-				
+
 			}
 		});
-	
-		ImageIcon icon5= new ImageIcon("resources/icons/exit.png");
-		JLabel tic= new JLabel("Ticks: ");
-		//empieza desde 1, pq es el valor minimo que puedes añadir a los ticks
-		spinTicks= new JSpinner (new SpinnerNumberModel(1,1,1000,1));
-		
-		this.btnExit = new JButton(icon5);
+
+		// ImageIcon icon5 = new ImageIcon("resources/icons/exit.png");
+		JLabel tic = new JLabel("Ticks: ");
+		// empieza desde 1, pq es el valor minimo que puedes añadir a los ticks
+		spinTicks = new JSpinner(new SpinnerNumberModel(1, 1, 1000, 1));
+
+		this.btnExit = new JButton(new ImageIcon("resources/icons/exit.png"));
 		this.btnExit.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				exit();
-				
+
 			}
 		});
-		
-		
+
 		this.add(btnCargaEventos);
 		this.add(btnCambiaClase);
 		this.add(btnCambiaTiempo);
 		this.add(btnPlay);
 		this.add(btnStop);
-		this.add(btnExit);
 		this.add(tic);
 		this.add(spinTicks);
-		
+		this.add(btnExit);
+
 	}
 
-	//TODO no funciona bien, solo se muestra la primera vez que se da al play
+	// TODO no funciona bien, solo se muestra la primera vez que se da al play
 	private void run_sim(int n) {
-		//System.out.println(_stopped);
+		// System.out.println(_stopped);
 		if (n > 0 && !_stopped) {
 			try {
-				_ctrl.run(1,null);
+				_ctrl.run(1, null);
 				Thread.sleep(1000);
 				enableToolBar(false);
 			} catch (Exception e) {
@@ -168,7 +172,7 @@ public class ControlPanel extends JPanel implements TrafficSimObserver{
 				@Override
 				public void run() {
 					run_sim(n - 1);
-					
+
 				}
 			});
 		} else {
@@ -178,7 +182,7 @@ public class ControlPanel extends JPanel implements TrafficSimObserver{
 	}
 
 	private void enableToolBar(boolean b) {
-		
+
 		this.btnCargaEventos.setEnabled(b);
 		this.btnCambiaClase.setEnabled(b);
 		this.btnCambiaTiempo.setEnabled(b);
@@ -190,110 +194,120 @@ public class ControlPanel extends JPanel implements TrafficSimObserver{
 	private void stop() {
 		_stopped = true;
 		enableToolBar(true);
-		
+
 	}
 
 	private void cargaEventos() throws IOException {
-		//TODO lanzar excepción
+		// TODO lanzar excepción
 		JFileChooser fc = new JFileChooser();
+		String nombreFichero;
 		int respuesta = fc.showOpenDialog(this);
+
 		if (respuesta == JFileChooser.APPROVE_OPTION) {
 			File archivoElegido = fc.getSelectedFile();
-	//		System.out.println(archivoElegido.getName());
-			_ctrl.reset();
-			InputStream in = new FileInputStream(archivoElegido);
-			_ctrl.loadEvents(in);
-			
+			nombreFichero = fc.getSelectedFile().getName();
+
+			if (nombreFichero.substring(nombreFichero.lastIndexOf("."), nombreFichero.length())
+					.equalsIgnoreCase(".json")) {
+				_ctrl.reset();
+				InputStream in = new FileInputStream(archivoElegido);
+				_ctrl.loadEvents(in);
+			} else
+				throw new IOException("Archivo con formato erroneo");
+
 		}
-		
+
 	}
 
-	
 	private void cambiaClase() {
-		listV= map.getVehicles();
-		ChangeCO2ClassDialog myCo2= new ChangeCO2ClassDialog(listV);
-		int res=myCo2.showConfirmDialog("Change co2 class");
+		listV = map.getVehicles();
+		ChangeCO2ClassDialog myCo2 = new ChangeCO2ClassDialog(listV);
+		int res = myCo2.showConfirmDialog("Change co2 class");
 		if (res == 0) {
-			//crear un evento nuevo del tipo setContClass
-	//		System.out.println(myCo2.getComboCo2()+"/"+myCo2.getComboV()+"/"+myCo2.getTic());
+			// crear un evento nuevo del tipo setContClass
+			// System.out.println(myCo2.getComboCo2()+"/"+myCo2.getComboV()+"/"+myCo2.getTic());
 			Pair<String, Integer> p = new Pair<String, Integer>(myCo2.getComboV(), myCo2.getComboCo2());
-			List<Pair<String, Integer>> cs = new ArrayList<Pair<String,Integer>>();
-			cs.add(p);
-			Event e = new SetContClassEvent(ticks+myCo2.getTic(), cs);
-			_ctrl.addEvent(e);
-		} 
-			
+			List<Pair<String, Integer>> cs = new ArrayList<Pair<String, Integer>>();
+
+			if (p != null && cs != null) {
+				cs.add(p);
+				Event e = new SetContClassEvent(ticks + myCo2.getTic(), cs);
+				_ctrl.addEvent(e);
+			} else
+				throw new NullPointerException("Error al recoger los datos de ChangeCO2ClassDialog.");
+		}
+
 	}
 
 	private void cambiaTiempo() {
-		listR= map.getRoads();
-		ChangeWeatherDialog myWea= new ChangeWeatherDialog(listR);
-		
-		int res=myWea.showConfirmDialog("Change weather class"); 
+		listR = map.getRoads();
+		ChangeWeatherDialog myWea = new ChangeWeatherDialog(listR);
+
+		int res = myWea.showConfirmDialog("Change weather class");
 		if (res == 0) {
-			//crear evento de set weather
-		//	System.out.println(myWea.getComboR()+"/"+myWea.getComboWea()+"/"+myWea.getTic());
+			// crear evento de set weather
+			// System.out.println(myWea.getComboR()+"/"+myWea.getComboWea()+"/"+myWea.getTic());
 			Pair<String, Weather> p = new Pair<String, Weather>(myWea.getComboR(), myWea.getComboWea());
-			List<Pair<String, Weather>> cs = new ArrayList<Pair<String,Weather>>();
-			cs.add(p);
-			Event e = new SetWeatherEvent(ticks+myWea.getTic(), cs);
-			_ctrl.addEvent(e);
-		} 
+			List<Pair<String, Weather>> cs = new ArrayList<Pair<String, Weather>>();
+			if (p != null && cs != null) {
+				cs.add(p);
+				Event e = new SetWeatherEvent(ticks + myWea.getTic(), cs);
+				_ctrl.addEvent(e);
+			} else
+				throw new NullPointerException("Error al recoger los datos de ChangeWeatherDialog.");
+		}
 	}
 
-	//Posible cambio cuando se ejecute
+	// Posible cambio cuando se ejecute
 	private void exit() {
-		 int confirm = JOptionPane.showOptionDialog(
-	             null, "¿Quieres salir de la aplicacion?", 
-	             "Confirmacion de salida", JOptionPane.YES_NO_OPTION, 
-	             JOptionPane.QUESTION_MESSAGE, null, null, null);
-	        if (confirm == 0) {
-	           System.exit(0);
-	        }
+		int confirm = JOptionPane.showOptionDialog(null, "¿Quieres salir de la aplicacion?", "Confirmacion de salida",
+				JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+		if (confirm == 0) {
+			System.exit(0);
+		}
 	}
 
 	private void update(RoadMap map, List<Event> events, int time) {
-		this.map= map;
-		this.listE= events;
-		this.ticks= time;
-		
-	}
-	
-	@Override
-	public void onAdvanceStart(RoadMap map, List<Event> events, int time) {
-		update( map,  events, time);
-		
+		this.map = map;
+		this.listE = events;
+		this.ticks = time;
+
 	}
 
+	@Override
+	public void onAdvanceStart(RoadMap map, List<Event> events, int time) {
+		update(map, events, time);
+
+	}
 
 	@Override
 	public void onAdvanceEnd(RoadMap map, List<Event> events, int time) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onEventAdded(RoadMap map, List<Event> events, Event e, int time) {
-		update(map,events,time);
-		
+		update(map, events, time);
+
 	}
 
 	@Override
 	public void onReset(RoadMap map, List<Event> events, int time) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onRegister(RoadMap map, List<Event> events, int time) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onError(String err) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
